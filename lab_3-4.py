afe6ec3ff302461d10c12fdd1f77c87524fa0e5b
p24lohara <p24lohara@fairfieldprep.org>
2023-10-04 21:08:18 -0400

    Lab 3-4 Submission

--- lab_3-4.py
+++ lab_3-4.py
@@ -14,6 +14,36 @@ Without using Python, determine the value of expressions. Use a comment to recor
  math.sin(0)
  
 Copy the syntax into your file and check your answers.
-
-
 """
+
+# Liam O'Hara
+import math
+math.ceil(7.15)
+# answer is 8
+
+math.floor(19.156)
+# answer is 18
+
+math.fabs(-42)
+# answer is 42
+
+math.pow(2, 3)
+# answer is 8
+
+math.log10(100)
+# answer is 10
+
+math.sqrt(9)
+# answer is 3
+
+math.isqrt(2)
+# answer is 1
+
+math.factorial(5)
+# answer is 120
+
+math.gcd(111111111, 12321)
+# answer is 333
+
+print(math.sin(0))
+# answer is 0
